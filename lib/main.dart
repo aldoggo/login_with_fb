21ca3ffcac97e5697e00d1fc46fbb8dda3e55d91
aldoggo <aldo.garcia@undostres.com.mx>
2022-10-21 16:02:58 -0500

    giving more information about user

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,148 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 void main() {
-  runApp(const FacebookApp());
+  // check if is running on Web
+  if (kIsWeb) {
+    // initialiaze the facebook javascript SDK
+    FacebookAuth.i.webInitialize(
+      appId: "1483671772099707",
+      cookie: true,
+      xfbml: true,
+      version: "v13.0",
+    );
+  }
+  runApp(const MyApp());
 }
 
-class FacebookApp extends StatefulWidget {
-  const FacebookApp({super.key});
+String prettyPrint(Map json) {
+  JsonEncoder encoder = const JsonEncoder.withIndent('  ');
+  String pretty = encoder.convert(json);
+  return pretty;
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<FacebookApp> createState() => _FacebookAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _FacebookAppState extends State<FacebookApp> {
-  Map? _userData;
+class _MyAppState extends State<MyApp> {
+  Map<String, dynamic>? _userData;
+  AccessToken? _accessToken;
+  bool _checking = true;
+
+  @override
+  void get initState {
+    super.initState;
+    _checkIfIsLogged();
+  }
+
+  Future<void> _checkIfIsLogged() async {
+    final accessToken = await FacebookAuth.instance.accessToken;
+    setState(() {
+      _checking = false;
+    });
+    if (accessToken != null) {
+      print("is Logged:::: ${prettyPrint(accessToken.toJson())}");
+      // now you can call to  FacebookAuth.instance.getUserData();
+      final userData = await FacebookAuth.instance.getUserData();
+      // final userData = await FacebookAuth.instance.getUserData(fields: "email,birthday,friends,gender,link");
+      _accessToken = accessToken;
+      setState(() {
+        _userData = userData;
+      });
+    }
+  }
+
+  void _printCredentials() {
+    print(
+      prettyPrint(_accessToken!.toJson()),
+    );
+  }
+
+  Future<void> _login() async {
+    final LoginResult result = await FacebookAuth.instance
+        .login(); // by default we request the email and the public profile
+
+    // loginBehavior is only supported for Android devices, for ios it will be ignored
+    // final result = await FacebookAuth.instance.login(
+    //   permissions: ['email', 'public_profile', 'user_birthday', 'user_friends', 'user_gender', 'user_link'],
+    //   loginBehavior: LoginBehavior
+    //       .DIALOG_ONLY, // (only android) show an authentication dialog instead of redirecting to facebook app
+    // );
+
+    if (result.status == LoginStatus.success) {
+      _accessToken = result.accessToken;
+      _printCredentials();
+      // get the user data
+      // by default we get the userId, email,name and picture
+      final userData = await FacebookAuth.instance.getUserData();
+      // final userData = await FacebookAuth.instance.getUserData(fields: "email,birthday,friends,gender,link");
+      _userData = userData;
+    } else {
+      print(result.status);
+      print(result.message);
+    }
+
+    setState(() {
+      _checking = false;
+    });
+  }
+
+  Future<void> _logOut() async {
+    await FacebookAuth.instance.logOut();
+    _accessToken = null;
+    _userData = null;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Facebook (Logged ${_userData == null ? 'out' : 'in'})'),
+          title: const Text('Facebook Auth Example'),
         ),
-        body: Center(
-            child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: () async {
-                  final result = await FacebookAuth.instance
-                      .login(permissions: ["public_profile", "email"]);
-
-                  if (result.status == LoginStatus.success) {
-                    final userData = await FacebookAuth.instance.getUserData(
-                      fields: "email,name",
-                    );
-
-                    setState(() {
-                      _userData = userData;
-                    });
-                  }
-                },
-                child: const Text('Log In')),
-            ElevatedButton(
-                onPressed: () async {
-                  await FacebookAuth.instance.logOut();
-
-                  setState(() {
-                    _userData = null;
-                  });
-                },
-                child: const Text('Log Out')),
-          ],
-        )),
+        body: _checking
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        _userData != null
+                            ? prettyPrint(_userData!)
+                            : "NO LOGGED",
+                      ),
+                      const SizedBox(height: 20),
+                      _accessToken != null
+                          ? Text(
+                              prettyPrint(_accessToken!.toJson()),
+                            )
+                          : Container(),
+                      const SizedBox(height: 20),
+                      CupertinoButton(
+                        color: Colors.blue,
+                        onPressed: _userData != null ? _logOut : _login,
+                        child: Text(
+                          _userData != null ? "LOGOUT" : "LOGIN",
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                    ],
+                  ),
+                ),
+              ),
       ),
     );
   }
